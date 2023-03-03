e5fe5fff13f61fa335ed420e59ae961a6aee978f
Thanh-Van-2001 <felicenguyen2001@gmail.com>
2023-03-03 22:37:55 +0700

    custom components/footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import {Row} from "antd";
+import {Col, Row} from "antd";
 import {withTranslation} from "react-i18next";
 import {SvgIcon} from "@/common/SvgIcon";
 import Container from "../../common/Container";
@@ -38,32 +38,98 @@ const Footer = ({t}: any): JSX.Element => {
                         justify="space-between"
                         align="middle"
                         style={{paddingTop: "3rem"}}
+
+                        
                     >
-                        <NavLink to="/">
+                        <Col lg={8} md={12} sm={12} xs={24}>
+                        <NavLink to="#">
                             <LogoContainer>
-                                Social Recovery
+                               ABOUT US
                             </LogoContainer>
                         </NavLink>
+                        <NavLink to="#">
+                            <LogoContainer>
+                               Our Documentations
+                            </LogoContainer>
+                        </NavLink>
+                    
+                    
+                        <NavLink to="#">
+                            <LogoContainer>
+                               Our pitch deck
+                            </LogoContainer>
+                        </NavLink>
+                    
                         <FooterContainer>
                             <SocialLink
-                                href="https://github.com/Adrinlol/create-react-app-adrinlol"
-                                src="github.svg"
+                                href="https://github.com/TCT-Hackathon-Team"
+                                src="githubbb.svg"
                             />
                             <SocialLink
-                                href="https://twitter.com/Adrinlolx"
-                                src="twitter.svg"
+                                href="#"
+                                src="bluefb.svg"
                             />
                             <SocialLink
-                                href="https://www.linkedin.com/in/lasha-kakabadze/"
-                                src="linkedin.svg"
+                                href="#"
+                                src="twitt.svg"
                             />
-                            <a href="https://fantomq12023.devpost.com/">
+                            <SocialLink
+                                href="#"
+                                src="linkedin-2.svg"
+                            />
+                            {/* <a href="https://fantomq12023.devpost.com/">
                                 <img
                                     src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=adrinlol&button_colour=FF5F5F&font_colour=ffffff&font_family=Lato&outline_colour=000000&coffee_colour=FFDD00"
                                     alt="Buy me a coffee"
                                 />
-                            </a>
+                            </a> */}
                         </FooterContainer>
+                        </Col>
+                        <Col lg={8} md={12} sm={12} xs={24}>
+                        <NavLink to="#">
+                            <LogoContainer>
+                               CONTACT US
+                            </LogoContainer>
+                        </NavLink>
+                        <NavLink to="#">
+                            <LogoContainer>
+                                144 Xuan Thuy, Cau Giay, Ha Noi
+                            </LogoContainer>
+                        </NavLink>
+                        <NavLink to="#">
+                            <LogoContainer>
+                                Hotline: 0962173195
+                            </LogoContainer>
+                        </NavLink>
+                        <NavLink to="#">
+                            <LogoContainer>
+                                felicenguyen2001@gmail.com
+                            </LogoContainer>
+                        </NavLink>
+                        </Col>
+                        <Col lg={8} md={12} sm={12} xs={24}>
+                        <NavLink to="#">
+                            <LogoContainer>
+                                ABOUT FANTOM
+                            </LogoContainer>
+                        </NavLink>
+                        <NavLink to="https://docs.fantom.foundation/smart-contract/deploy-a-smart-contract">
+                            <LogoContainer>
+                                Fantom Documentations
+                            </LogoContainer>
+                        </NavLink>
+                        <NavLink to="https://forum.fantom.network/">
+                            <LogoContainer>
+                               Fantom Forum Network
+                            </LogoContainer>
+                        </NavLink>
+                        <NavLink to="https://discord.com/invite/zS4Ek4WByA/">
+                            <LogoContainer>
+                               Fantom Offical Discord
+                            </LogoContainer>
+                        </NavLink>
+                        </Col>
+                        
                     </Row>
                 </Container>
             </Extra>
